bcb163f77c14a1cc194e9966894340616da7e96f
niya1123 <yoheikurioka@gmail.com>
2020-12-11 17:29:16 +0900

    参考文献辺りをbib使うようにした

--- pdf.bat
+++ pdf.bat
@@ -1,4 +1,7 @@
 docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
+docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja pbibtex %1
+docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
+docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
 docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
 docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja dvipdfmx %1.dvi
 del *.aux
@@ -7,3 +10,5 @@ del *.log
 del *.fls
 del *.toc
 del *.fdb_latexmk
+del *.bbl
+del *.blg
