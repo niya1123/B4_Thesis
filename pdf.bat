37e433daf2eabceba88dc37e71ef3f43c2f28b06
niya1123 <yoheikurioka@gmail.com>
2020-12-11 15:16:25 +0900

    引数に変更

--- pdf.bat
+++ pdf.bat
@@ -1,5 +1,5 @@
-docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex back.tex
-docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja dvipdfmx back.dvi
+docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
+docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja dvipdfmx %1.dvi
 del *.aux
 del *.dvi
 del *.log
