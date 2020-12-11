d2f23eaaff000516782efc23030d3ec59fb497a8
niya1123 <yoheikurioka@gmail.com>
2020-12-11 16:07:35 +0900

    thesisの追加とそれに伴う修正

--- pdf.bat
+++ pdf.bat
@@ -1,5 +1,9 @@
 docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
+docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja platex %1.tex
 docker run --rm -v "%cd%:/workdir" paperist/alpine-texlive-ja dvipdfmx %1.dvi
 del *.aux
 del *.dvi
 del *.log
+del *.fls
+del *.toc
+del *.fdb_latexmk
